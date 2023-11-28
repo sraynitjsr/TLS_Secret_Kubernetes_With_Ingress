ca14bd70076ea3cca36f26dcc72dbdec498168e4
Subhradeep Ray <35240569+sraynitjsr@users.noreply.github.com>
2023-11-28 17:13:58 +0530

    Deleting the Certificate, Private Key and CSR Files

--- tls-gen-script.sh
+++ tls-gen-script.sh
@@ -17,5 +17,7 @@ kubectl create secret tls my-secret-name \
 kubectl get secret your-secret-name -n my-namespace
 kubectl describe secret your-secret-name -n my-namespace
 
-# Step 4: After Getting Secret Ready, Let's Delete the Certificate If Not Needed Anymore
+# Step 4: After Getting Secret Ready, Let's Delete the Certificate, Private Key and CSR Files If Not Needed Anymore
 rm certificate.crt
+rm private.key
+rm csr.pem
